b26e213bfb638b4191ebb430bddb20c22a246d29
riskycheng <riskycheng@gmail.com>
2024-05-31 00:00:56 +0800

    initial commits

---
+++ ClockUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClockUp
+//
+//  Created by Jian Cheng on 2024/5/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
